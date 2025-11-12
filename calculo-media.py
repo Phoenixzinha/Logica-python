f2eb367b1cec3d208f8a2722d08d4b31d4b71b85
Giovanna <giovanna.s.ferreira08@gmail.com>
2025-11-12 14:39:16 -0300

    Delete calculo-media.py

--- calculo-media.py
+++
@@ -1,4 +0,0 @@
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-nota3 = float(input("Digite a terceira nota: "))
-print(f"A média das notas que você digitou é: {(nota1 + nota2 + nota3) / 3}")
